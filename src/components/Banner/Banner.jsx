70dc9581e7777de2c6a6d3b77a5d5a3cf65da81d
Rostyslav <rossgaluzinskiy@gmail.com>
2023-03-01 16:29:24 +0200

    add

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,6 @@
 import Carousel from 'react-bootstrap/Carousel';
 import banner1 from 'img/banner1.jpg';
 import banner2 from 'img/banner2.jpg';
-import banner3 from 'img/banner3.jpg';
-import banner4 from 'img/banner4.jpg';
 import { Slider } from './Banner.styled';
 
 function Banner() {
@@ -22,26 +20,6 @@ function Banner() {
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={banner3} alt="Third slide" />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={banner4} alt="Third slide" />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Banner from 'components/Banner';
 const Home = () => {
   return (
     <>
-      <h1>Welcome home</h1>
+      {/* <h1>Welcome home</h1> */}
       <Banner />
     </>
   );
